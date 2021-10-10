00ced23500d79356608bfa09518aed2075beb956
Kristian Aars <kristian@aars.me>
2021-10-10 19:02:29 +0200

    Added BFS-Traversing to the Graph-structure

--- src/no/ntnu/idi/krisvaa/idatt2101/Graph.java
+++ src/no/ntnu/idi/krisvaa/idatt2101/Graph.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Graph<T> {
@@ -16,31 +15,31 @@ public class Graph<T> {
     public Graph(int nodeCount, int edgeCount) {
         nodes = new Node[nodeCount];
 
-        for(int i = 0; i < nodes.length; i++) { nodes[i] = new Node<T>(); }
+        for(int i = 0; i < nodes.length; i++) { nodes[i] = new Node<T>(); nodes[i].id = i; }
 
         this.nodeCount = nodeCount;
         this.edgeCount = edgeCount;
     }
 
-    public static Graph readFromStream(InputStream is) throws IOException {
+    public static Graph<String> readFromStream(InputStream is) throws IOException {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int nodeCount = Integer.parseInt(st.nextToken());
             int edgeCount = Integer.parseInt(st.nextToken());
 
-            System.out.println(nodeCount + " " + edgeCount);
-
-            Graph g = new Graph<>(nodeCount, edgeCount);
+            Graph<String> g = new Graph<>(nodeCount, edgeCount);
 
             for(int i = 0; i < edgeCount; i++) {
                 st = new StringTokenizer(br.readLine());
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
 
-                System.out.println(from + " " + to);
-
                 Edge e = new Edge(g.nodes[to], g.nodes[from].edge1);
                 g.nodes[from].edge1 = e;
+
+                //Makes a redundant coupling to make the graph non-directional
+                e = new Edge(g.nodes[from], g.nodes[to].edge1);
+                g.nodes[to].edge1 = e;
             }
 
             return g;
@@ -51,22 +50,69 @@ public class Graph<T> {
         }
     }
 
+    public void bfs(Node<?> s) {
+        initPredecessor(s);
+
+        Queue<Node<?>> queue = new Queue<>(nodeCount - 1);
+        queue.addToQueue(s);
+
+        while (!queue.empty()) {
+            Node<?> n = queue.getNextInQueue();
+
+            for(Edge e = n.edge1; e != null; e = e.next) {
+                Predecessor p = (Predecessor)e.to.nodeData;
+
+                if(p.distance == Predecessor.infinity) {
+                    p.distance = ((Predecessor) n.nodeData).distance + 1;
+                    p.predecessor = n;
+                    queue.addToQueue(e.to);
+                }
+            }
+        }
+
+    }
+
+    private void initPredecessor(Node<?> s) {
+        for(Node n : nodes) {
+            n.nodeData = new Predecessor();
+        }
+        ((Predecessor) s.nodeData).distance = 0;
+    }
+
 }
 
 class Node<T> {
     Edge edge1;
     T data;
-
+    Object nodeData;
+    int id;
 }
 
 class Edge {
-
      Edge next;
      Node to;
 
-     public Edge(Node n, Edge next) {
+     public Edge(Node<?> n, Edge next) {
          this.to = n;
          this.next = next;
      }
+}
 
+class Predecessor {
+    public static final int infinity = 100000000;
+
+    int distance;
+    Node<?> predecessor;
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Node<?> findPredecessor() {
+        return predecessor;
+    }
+
+    public Predecessor() {
+        distance = infinity;
+    }
 }
--- src/no/ntnu/idi/krisvaa/idatt2101/Main.java
+++ src/no/ntnu/idi/krisvaa/idatt2101/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        Graph g = null;
+        //Load graph-data from file
+        Graph<String> g = null;
         try {
             g = Graph.readFromStream(new URL("http://www.iie.ntnu.no/fag/_alg/uv-graf/L7g1").openStream());
 
@@ -16,6 +17,26 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println(g);
+        //BFS-Traversing
+        int startNode = 5;
+        System.out.printf("BFS-Traversing with root-node %s: \n", startNode);
+
+        g.bfs(g.nodes[startNode]);
+        System.out.printf("%4s  %12s  %8s \n", "Node", "predecessor", "distance");
+        for(Node n : g.nodes) {
+            Predecessor nodeData = (Predecessor) n.nodeData;
+
+            int nodeID = n.id;
+
+            String predecessor = "";
+            if(nodeData.findPredecessor() != null) {
+                predecessor = "" + nodeData.findPredecessor().id;
+            }
+
+            int distance = ((Predecessor) n.nodeData).getDistance();
+            System.out.printf("%4s  %12s  %8s \n", nodeID, predecessor, distance);
+        }
+
+
     }
 }
